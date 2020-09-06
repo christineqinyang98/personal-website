7f98112d24dde87d1c71d5636468808137dffb60
christineqinyang98 <christineisonthejob@hotmail.com>
2020-09-06 00:25:37 -0400

    updated about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,53 +1,54 @@
 import React, { Component } from 'react';
+import ImageGallery from 'react-image-gallery';
+
+const images = [
+    {
+        original: 'images/about.jpg',
+    },
+    {
+        original: "images/Western.jpg",
+    },
+    {
+        original: "images/musicbox.jpg",
+    }
+];
+
 
 class About extends Component {
   render() {
 
     if(this.props.data){
-      var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.province;
-      var country = this.props.data.address.country;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
+        var aboutpic = "images/about.jpg";
+        var bio = this.props.data.bio;
+        var hobbies = this.props.data.hobbies;
+        var hobbiespic = "images/hobbies.jpg";
     }
 
     return (
       <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Christine Yang Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
-            <p>{bio}</p>
-
+            <h1>About Me</h1>
             <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-                     <span>{name}</span><br />
-                     <span>{city}, {state}, {country}</span> <br />
-					 <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
+                <div className="six columns">
+                    <img className="about-pic" src={aboutpic} alt="stock image of relaxation" />
+                </div>
+                <div className="six columns main-col">
+                    <h2>Introduction</h2>
+                    <p>{bio}</p>
+                </div>
             </div>
-         </div>
-      </div>
-
-   </section>
+            <div className="row 2">
+                <div className="six columns main-col">
+                    <h2 className="hobbies">Hobbies/Interests</h2>
+                    <p className="hobbies">{hobbies}</p>
+                </div>
+                <div className="six columns">
+                    <img className="hobbies-pic" src={hobbiespic} alt="stock image of relaxation" />
+                </div>
+            </div>
+      </section>
     );
   }
 }
 
 export default About;
+
